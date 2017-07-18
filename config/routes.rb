5b47d44a13c970e9ddd95b444e7ec7d901b35c5c
zulkarnay <bedelbaev@gmail.com>
2017-07-18 15:05:20 +0600

    add Question controller: destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :questions, except: [:destroy] do
+  resources :questions do
     resources :answers, only: [:create, :edit, :update]
   end
 
