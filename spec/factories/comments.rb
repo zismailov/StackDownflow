a60409e6e93892c7ed969411e5c84797f03e255c
zulkarnay <bedelbaev@gmail.com>
2017-07-25 13:29:08 +0600

    use concern in routes

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
   factory :question_comment, class: "Comment" do
     body "This is a question comment, yo!"
-    association :commentable, factory: :question
+    association :commentable
     user
   end
 
   factory :answer_comment, class: "Comment" do
     body "This is an answer comment, yo!"
-    association :commentable, factory: :answer
+    association :commentable
     user
   end
 end
--- spec/features/comment/delete_question_comment_spec.rb
+++ spec/features/comment/delete_question_comment_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.feature "Delete Question Comment", type: :feature do
   let(:user) { create(:user) }
   let(:user2) { create(:user) }
-  let(:question) { create(:question, user: user) }
+  let!(:question) { create(:question, user: user) }
   let!(:comment) { create(:question_comment, user: user, commentable: question) }
   let!(:comment2) { create(:question_comment, user: user2, commentable: question) }
 
@@ -13,7 +13,7 @@ RSpec.feature "Delete Question Comment", type: :feature do
   end
 
   scenario "User deletes his question comment", js: true do
-    within(".comment[data-comment-id='#{comment.id}']") do
+    within(".question .comment[data-comment-id='#{comment.id}']") do
       click_on "delete"
     end
     page.driver.browser.switch_to.alert.accept
@@ -22,7 +22,7 @@ RSpec.feature "Delete Question Comment", type: :feature do
   end
 
   scenario "User can't delete not his question comment", js: true do
-    within(".comment[data-comment-id='#{comment2.id}']") do
+    within(".question .comment[data-comment-id='#{comment2.id}']") do
       expect(page).not_to have_content "delete"
     end
   end
