b24eb908da995faab21725b1a9e0856b7854c7ce
zulkarnay <bedelbaev@gmail.com>
2017-08-09 14:49:22 +0600

    refactor controller: remove duplicating code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,30 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  def find_parent
+    resource, id = request.path.split("/")[1, 2]
+    @parent = resource.singularize.classify.constantize.find(id)
+  end
+
+  def update_resource(resource)
+    if resource.update(send(:"#{resource.class.to_s.downcase}_params"))
+      flash.now[:success] = "#{resource.class} is edited!"
+      render json: resource, root: false, status: 200
+    else
+      flash.now[:danger] = "#{resource.class} is not edited! See errors below."
+      render json: resource.errors.as_json, status: :unprocessable_entity
+    end
+  end
+
+  def destroy_resource(resource)
+    resource.destroy
+    flash.now[:success] = "#{resource.class} is deleted!"
+    render json: :nothing, status: 204
   end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,38 +1,27 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_commentable
   before_action :set_comment, only: [:edit, :update, :destroy]
   before_action :comment_belongs_to_current_user?, only: [:edit, :update, :destroy]
 
-  respond_to :json, only: [:create, :update, :destroy]
-
   def create
-    @comment = @commentable.comments.new(comment_params)
+    @comment = @parent.comments.new(comment_params)
     @comment.user = current_user
 
     if @comment.save
       flash.now[:success] = "Comment is created!"
-      respond_with @comment, location: nil, root: false
+      render json: @comment, status: 201, root: false
     else
       flash.now[:danger] = "Invalid data! Comment length should be more than 10 symbols!"
-      respond_with(@comment.errors.as_json, status: :unprocessable_entity, location: nil)
+      render json: @comment.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    if @comment.update(comment_params)
-      flash.now[:success] = "Comment is edited!"
-      render json: @comment, root: false
-    else
-      flash.now[:danger] = "Comment is not edited! See errors below."
-      render json: @comment.errors.as_json, status: :unprocessable_entity
-    end
+    update_resource @comment
   end
 
   def destroy
-    @comment.destroy
-    flash.now[:success] = "Comment is deleted!"
-    respond_with :nothing, status: 204
+    destroy_resource @comment
   end
 
   private
@@ -41,22 +30,8 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:body)
   end
 
-  # rubocop:disable Metrics/AbcSize
-  def set_commentable
-    if !params[:answer_id].nil?
-      @commentable = Answer.find(params[:answer_id])
-      @return_path = @commentable.question
-    elsif !params[:question_id].nil?
-      puts "$$$$ Question #{Question.count}"
-      puts "$$$$ Question #{Question.first.inspect}"
-      @commentable = Question.find(params[:question_id])
-      @return_path = @commentable
-    end
-  end
-
   def set_comment
-    puts "$$$$ Comment #{Comment.count}"
-    @comment = @commentable.comments.find(params[:id])
+    @comment = Comment.find(params[:id])
   end
 
   def comment_belongs_to_current_user?
