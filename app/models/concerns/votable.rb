25e3c87135e79780eb8865576af57df88f65c792
zulkarnay <bedelbaev@gmail.com>
2017-08-09 14:37:42 +0600

    refactor Votable methods

---
+++ app/models/concerns/votable.rb
@@ -0,0 +1,23 @@
+module Votable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :votes, as: :votable, dependent: :destroy
+  end
+
+  def vote_up(user)
+    votes.create(user_id: user.id, vote: 1) unless voted_by? user
+  end
+
+  def vote_down(user)
+    votes.create(user_id: user.id, vote: -1) unless voted_by? user
+  end
+
+  def total_votes
+    votes.inject(0) { |s, v| s + v.vote }
+  end
+
+  def voted_by?(user)
+    votes.find_by_user_id(user) ? true : false
+  end
+end
