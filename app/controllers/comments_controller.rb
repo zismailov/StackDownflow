5e93a3ae2959e65245a57422daf443ed83a89310
zulkarnay <bedelbaev@gmail.com>
2017-08-29 15:16:24 +0600

    add comment live updating

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,19 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_comment, only: [:edit, :update, :destroy]
   before_action :comment_belongs_to_current_user?, only: [:edit, :update, :destroy]
+  after_action :publish_after_destroy, only: [:destroy]
 
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/MethodLength
   def create
     @comment = @parent.comments.new(comment_params)
     @comment.user = current_user
 
     if @comment.save
       flash.now[:success] = "Comment is created!"
+      PrivatePub.publish_to "/questions/#{@parent.class.name == 'Question' ? @parent.id : @parent.question.id}",
+                            comment_create: CommentSerializer.new(@comment, root: false).to_json,
+                            parent: @parent.class.name, parent_id: @parent.id
       render json: @comment, status: 201, root: false
     else
       flash.now[:danger] = "Invalid data! Comment length should be more than 10 symbols!"
@@ -36,5 +42,12 @@ class CommentsController < ApplicationController
 
   def comment_belongs_to_current_user?
     redirect_to root_path, status: 403 unless @comment.user == current_user
+  end
+
+  # rubocop:disable LineLength
+  def publish_after_destroy
+    PrivatePub.publish_to "/questions/#{@comment.commentable.class.name == 'Question' ? @comment.commentable.id : @comment.commentable.question.id}",
+                          comment_destroy: @comment.id, parent: @comment.commentable.class.name,
+                          parent_id: @comment.commentable.id
   end
 end
