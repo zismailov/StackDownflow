8a2c21ea8733f633a13da04ce0caddb477aacfea
zulkarnay <bedelbaev@gmail.com>
2017-08-31 16:30:15 +0600

    fix votes controller spec

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -5,25 +5,26 @@ RSpec.describe VotesController, type: :controller do
   let(:user2) { create(:user) }
   let(:question) { create(:question, user: user) }
   let(:answer) { create(:answer, question: question, user: user) }
-  let(:answer2) { create(:answer, question: question, user: user2) }
+  let(:comment) { create(:answer_comment, commentable: answer, user: user) }
+  let(:comment2) { create(:answer_comment, commentable: answer, user: user2) }
 
   describe "PATCH #vote_up" do
     let(:patch_vote_up) do
-      patch :vote_up, params: { id: answer2 }, format: :json
-      answer2.reload
+      patch :vote_up, params: { id: comment2 }, format: :json
+      comment2.reload
     end
 
     context "as an authenticated user" do
-      context "when answer doesn't belong to current user" do
-        it "increases answer's votes" do
+      context "when comment doesn't belong to current user" do
+        it "increases comment's votes" do
           sign_in user
-          expect { patch_vote_up }.to change(answer2, :total_votes).by(1)
+          expect { patch_vote_up }.to change(comment2, :total_votes).by(1)
         end
 
-        it "sets answer's votes" do
+        it "sets comment's votes" do
           sign_in user
           patch_vote_up
-          expect(answer2.total_votes).to eq 1
+          expect(comment2.total_votes).to eq(1)
         end
 
         it "returns status 200" do
@@ -39,14 +40,14 @@ RSpec.describe VotesController, type: :controller do
         end
       end
 
-      context "when answer belongs to current user" do
+      context "when comment belongs to current user" do
         let(:patch_vote_up) do
-          post :vote_up, params: { id: answer }, format: :json
+          post :vote_up, params: { id: comment }, format: :json
         end
 
-        it "doesn't increase answer's votes" do
+        it "doesn't increase comment's votes" do
           sign_in user
-          expect { patch_vote_up }.not_to change(answer, :total_votes)
+          expect { patch_vote_up }.not_to change(comment, :total_votes)
         end
 
         it "returns status 403" do
@@ -58,8 +59,8 @@ RSpec.describe VotesController, type: :controller do
     end
 
     context "as an guest user" do
-      it "doesn't increase answer's votes" do
-        expect { patch_vote_up }.not_to change(answer2, :total_votes)
+      it "doesn't increase comment's votes" do
+        expect { patch_vote_up }.not_to change(comment2, :total_votes)
       end
 
       it "returns 401 status code" do
@@ -69,23 +70,23 @@ RSpec.describe VotesController, type: :controller do
     end
   end
 
-  describe "GET #vote_down" do
+  describe "GET vote_down" do
     let(:patch_vote_down) do
-      patch :vote_down, params: { id: answer2 }, format: :json
-      answer2.reload
+      patch :vote_down, params: { id: comment2 }, format: :json
+      comment2.reload
     end
 
     context "as an authenticated user" do
-      context "when answer doesn't belong to current user" do
-        it "decreases answer's votes" do
+      context "when comment doesn't belong to current user" do
+        it "decreases comment's votes" do
           sign_in user
-          expect { patch_vote_down }.to change(answer2, :total_votes).by(-1)
+          expect { patch_vote_down }.to change(comment2, :total_votes).by(-1)
         end
 
-        it "sets answer's votes" do
+        it "sets comment's votes" do
           sign_in user
           patch_vote_down
-          expect(answer2.reload.total_votes).to eq(-1)
+          expect(comment2.reload.total_votes).to eq(-1)
         end
 
         it "returns status 200" do
@@ -101,14 +102,14 @@ RSpec.describe VotesController, type: :controller do
         end
       end
 
-      context "when answer belongs to current user" do
+      context "when comment belongs to current user" do
         let(:patch_vote_down) do
-          post :vote_down, params: { id: answer }, format: :json
+          post :vote_down, params: { id: comment }, format: :json
         end
 
-        it "doesn't increase answer's votes" do
+        it "doesn't increase comment's votes" do
           sign_in user
-          expect { patch_vote_down }.not_to change(answer, :total_votes)
+          expect { patch_vote_down }.not_to change(comment, :total_votes)
         end
 
         it "returns status 403" do
@@ -119,8 +120,8 @@ RSpec.describe VotesController, type: :controller do
       end
     end
     context "as an guest user" do
-      it "doesn't increase answer's votes" do
-        expect { patch_vote_down }.not_to change(answer2, :total_votes)
+      it "doesn't increase comment's votes" do
+        expect { patch_vote_down }.not_to change(comment2, :total_votes)
       end
 
       it "returns 401 status code" do
