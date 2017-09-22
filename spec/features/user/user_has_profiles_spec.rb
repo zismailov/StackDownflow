410267fe544b3d02f91cddea19dc8d0d6fe524e7
zulkarnay <bedelbaev@gmail.com>
2017-09-22 19:17:48 +0600

    add profiles for users

---
+++ spec/features/user/user_has_profiles_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.feature "Registered User Has Profile", type: :feature do
+  let!(:user) { create(:user) }
+
+  background { sign_in user }
+
+  scenario "User can visit his profile" do
+    visit user_path(user.username)
+
+    expect(page).to have_content user.username
+  end
+end
