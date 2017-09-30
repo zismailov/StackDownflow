49e8b5992a67854afc7ba3c41a664364ea47e436
zulkarnay <bedelbaev@gmail.com>
2017-09-30 15:13:12 +0600

    add API for Questions#create and Answers#create

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
-  authorize_resource
+  skip_authorization_check
 
   def index
     respond_with @questions = Question.all
@@ -7,5 +7,15 @@ class Api::V1::QuestionsController < Api::V1::BaseController
 
   def show
     respond_with @question = Question.find(params[:id]), serializer: QuestionShowSerializer
+  end
+
+  def create
+    respond_with @question = current_resource_owner.questions.create(question_params)
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body, :tag_list)
   end
 end
